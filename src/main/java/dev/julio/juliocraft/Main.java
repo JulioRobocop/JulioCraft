ae027366f27a9142c29edd119b7c7eeefd90b137
JulioRobocop <juliocesar.gp44@gmail.com>
2026-08-14 16:51:07 -0300

    refactor: Simplify and add IAppLogic methods

--- src/main/java/dev/julio/juliocraft/Main.java
+++ src/main/java/dev/julio/juliocraft/Main.java
@@ -1,33 +1,29 @@
 package dev.julio.juliocraft;
 
+import dev.julio.juliocraft.core.Engine;
+import dev.julio.juliocraft.core.IAppLogic;
 import dev.julio.juliocraft.core.Window;
-import org.lwjgl.Version;
-import org.lwjgl.glfw.GLFW;
-import org.lwjgl.glfw.GLFW.*;
+import dev.julio.juliocraft.core.graph.Render;
+import dev.julio.juliocraft.core.scene.Scene;
 
-public class Main {
+public class Main implements IAppLogic {
     public static void main(String[] args) {
-        double prevTime = 0.0;
-        double crntTime = 0.0;
-        double timeDiff;
-        int counter = 0;
-        System.out.println(Version.getVersion());
-        Window window = new Window("Hello World", 1600, 900, false);
-        window.init();
-        while(!window.windowShouldClose()) {
-            crntTime = GLFW.glfwGetTime();
-            timeDiff = crntTime - prevTime;
-            counter++;
-            if (timeDiff >= 1.0) {
-                double FPS =   counter / timeDiff;
-                double ms =  (timeDiff / counter) * 1000.0;
-                window.setTitle(String.format("JulioCraft - %.0f FPS / %.2f ms", FPS, ms));
-                prevTime = crntTime;
-                counter = 0;
-            }
-            window.update();
-
-        }
-        window.cleanup();
+        Main main = new Main();
+        Engine gameEng = new Engine("JulioCraft", new Window.WindowOptions(), main);
+        gameEng.start();
     }
+
+    @Override
+    public void cleanup() {}
+
+    @Override
+    public void init(Window window, Scene scene, Render render) {}
+
+    @Override
+    public void input(Window window, Scene scene, long diffTimeMillis) {}
+
+    @Override
+    public void update(Window window, Scene scene, long diffTimeMillis) {}
+
+
 }
