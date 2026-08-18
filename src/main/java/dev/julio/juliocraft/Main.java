c7ced0dfa7ae020cb9bf3b18648ebc111fa754f5
JulioRobocop <juliocesar.gp44@gmail.com>
2026-08-18 16:45:11 -0300

    feat: Add quad positions and colors to generate an object on the screen

--- src/main/java/dev/julio/juliocraft/Main.java
+++ src/main/java/dev/julio/juliocraft/Main.java
@@ -2,6 +2,7 @@ package dev.julio.juliocraft;
 
 import dev.julio.juliocraft.core.Engine;
 import dev.julio.juliocraft.core.IAppLogic;
+import dev.julio.juliocraft.core.Mesh;
 import dev.julio.juliocraft.core.Window;
 import dev.julio.juliocraft.core.graph.Render;
 import dev.julio.juliocraft.core.scene.Scene;
@@ -17,13 +18,30 @@ public class Main implements IAppLogic {
     public void cleanup() {}
 
     @Override
-    public void init(Window window, Scene scene, Render render) {}
+    public void init(Window window, Scene scene, Render render) {
+        float[] positions = new float[] {
+                -0.5f, 0.5f, 0.0f,
+                -0.5f, -0.5f, 0.0f,
+                0.5f, -0.5f, 0.0f,
+                0.5f, 0.5f, 0.0f,
+        };
+        float[] colors = new float[] {
+                0.5f, 0.0f, 0.0f,
+                0.0f, 0.5f, 0.0f,
+                0.0f, 0.0f, 0.5f,
+                0.0f, 0.5f, 0.5f
+        };
+        int[] indices = new int[] {
+                0, 1, 3, 3, 1, 2
+        };
+        Mesh mesh = new Mesh(positions, colors, indices);
+        scene.addMesh("quad", mesh);
+    }
 
     @Override
     public void input(Window window, Scene scene, long diffTimeMillis) {}
 
     @Override
     public void update(Window window, Scene scene, long diffTimeMillis) {}
-
 
 }
