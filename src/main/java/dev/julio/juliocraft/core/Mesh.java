e830b4ec446bf25bd27278212a477963afd02ea5
JulioRobocop <juliocesar.gp44@gmail.com>
2026-08-18 16:45:57 -0300

    feat: Add VBO colors and index buffers

--- src/main/java/dev/julio/juliocraft/core/Mesh.java
+++ src/main/java/dev/julio/juliocraft/core/Mesh.java
@@ -4,6 +4,7 @@ import org.lwjgl.opengl.GL30;
 import org.lwjgl.system.MemoryUtil;
 
 import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,13 +16,14 @@ public class Mesh {
     private int vaoId;
     private List<Integer> vboIdList;
 
-    public Mesh(float[] positions, int numVertices) {
-        this.numVertices = numVertices;
+    public Mesh(float[] positions, float[] colors, int[] indices) {
+        numVertices = indices.length;
         vboIdList = new ArrayList<>();
 
         vaoId = glGenVertexArrays();
         glBindVertexArray(vaoId);
 
+        // VBO Positions
         int vboId = glGenBuffers();
         vboIdList.add(vboId);
         FloatBuffer positionsBuffer = MemoryUtil.memCallocFloat(positions.length);
@@ -31,10 +33,30 @@ public class Mesh {
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(0, 3, GL_FLOAT, false, 0,0);
 
+        // VBO Color
+        vboId = glGenBuffers();
+        vboIdList.add(vboId);
+        FloatBuffer colorsBuffer = MemoryUtil.memCallocFloat(colors.length);
+        colorsBuffer.put(0, colors);
+        glBindBuffer(GL_ARRAY_BUFFER, vboId);
+        glBufferData(GL_ARRAY_BUFFER, colorsBuffer, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 3, GL_FLOAT, false, 0, 0);
+
+        // VBO Index
+        vboId = glGenBuffers();
+        vboIdList.add(vboId);
+        IntBuffer indexBuffer = MemoryUtil.memCallocInt(indices.length);
+        indexBuffer.put(0, indices);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboId);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexBuffer, GL_STATIC_DRAW);
+
         glBindBuffer(GL_ARRAY_BUFFER, 0);
         glBindVertexArray(0);
 
         MemoryUtil.memFree(positionsBuffer);
+        MemoryUtil.memFree(colorsBuffer);
+        MemoryUtil.memFree(indexBuffer);
     }
 
     public void cleanup() {
